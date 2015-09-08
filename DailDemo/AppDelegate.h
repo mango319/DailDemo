f06b980cac40fb2d1a90ca69f8e38a0370e00f00
胖子 <pangzi@pangzhideMacBook-Pro.local>
2015-09-08 14:03:01 +0800

    Initial Commit

---
+++ DailDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DailDemo
+//
+//  Created by 胖子 on 15/9/8.
+//  Copyright (c) 2015年 胖子. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DailDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DailDemo
+//
+//  Created by 胖子 on 15/9/8.
+//  Copyright (c) 2015年 胖子. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
